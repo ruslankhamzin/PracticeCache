14baa2cb2a1025bd8cbe3395f7c31ece7d135c06
Ruslan Khamzin <hamzin362@gmail.com>
2022-07-30 02:20:19 +0400

    Add first logic

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,43 @@
 package org.example;
 
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class App {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Write a size of cache: ");
+            int size = Integer.parseInt(reader.readLine());
+            System.out.println("Choose a repository (1-Disk,2-Memory): ");
+            int repo = Integer.parseInt(reader.readLine());
+            boolean onDisk = false;
+            if (repo == 1) {
+                onDisk = true;
+            }
+            RealizationCache myCache = new RealizationCache(onDisk, size);
+
+
+        while (true) {
+            System.out.println("Write a key(write delete to clear cache):");
+            String key = reader.readLine();
+            if (key.equals("delete")) {
+                myCache.clear();
+                continue;
+            }
+            if (myCache.get(key) == null) {
+                System.out.println("Write a value: ");
+                String value = reader.readLine();
+                myCache.put(key, value);
+                //System.out.println(myCache.map1.toString() + " " + myCache.map.toString());
+            } else {
+                System.out.println("Key: " + key + " Value: " + myCache.get(key).toString());
+            }
+        }
+        } catch (IOException e) {
+        System.out.println("Ошибка ввода-вывода");
+    }
     }
 }
