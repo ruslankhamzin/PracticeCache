684d57dec477e84aad20e5005c8e75d892a10cbb
Ruslan Khamzin <hamzin362@gmail.com>
2022-08-03 03:42:55 +0400

    Debug

---
+++ src/main/java/org/example/models/Key.java
@@ -0,0 +1,26 @@
+package org.example.models;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class Key {
+    String title;
+    LocalDateTime dateOfCreated;
+    Long id;
+
+    public Key(String title) {
+        this.title = title;
+        dateOfCreated = LocalDateTime.now();
+        Random random = new Random();
+        id = random.nextLong();
+    }
+
+    @Override
+    public String toString() {
+        return "Key{" +
+                "title='" + title + '\'' +
+                ", dateOfCreated=" + dateOfCreated +
+                ", id=" + id +
+                '}';
+    }
+}
