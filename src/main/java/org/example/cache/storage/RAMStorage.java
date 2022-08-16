2befb8bd895167132da287bcaf6238c1f611ff18
Ruslan Khamzin <hamzin362@gmail.com>
2022-08-16 16:29:57 +0400

    Debug

--- src/main/java/org/example/cache/storage/RAMStorage.java
+++ src/main/java/org/example/cache/storage/RAMStorage.java
@@ -1,41 +1,40 @@
 package org.example.cache.storage;
 
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.Map;
 
 public class RAMStorage<T, V> implements StorageStrategy<T, V> {
-    private final LinkedHashMap<T, V> values;
     private final int size;
+    public LinkedHashMap<T, V> values;
 
     public RAMStorage(int size) {
         this.size = size;
-        values = new LinkedHashMap<>();
+        values = new LinkedHashMap<T, V>(size) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return size < values.size();
+            }
+        };
     }
-
 
     @Override
     public void put(T key, V value) {
-        while (size <= values.size()) pruning();
         values.put(key, value);
     }
-
 
     @Override
     public V get(T key) {
         V value = values.get(key);
         values.remove(key);
         values.put(key, value);
-        return values.get(key);
+        return value;
     }
 
-
-    private void pruning() {
+    /*private void pruning() {
         List<T> keys = new ArrayList<>(values.keySet());
         values.remove(keys.get(0));
-    }
-
+    }*/
 
     @Override
     public void clear() {
